030cbed459dfd5b934b244f51af174946402d8a7
Shady <76508906+ShadyBarrios@users.noreply.github.com>
2021-08-23 00:22:11 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) throws Exception{
+		Parent parent = FXMLLoader.load(getClass().getResource("ManagementStudentSystem.fxml"));
+		Scene scene = new Scene(parent);
+		stage.setTitle("Management Student System");
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
+
+
